b9cf79d681eaf1571785b05423031b841a6e985e
DanielKell <danielkell.ubc@gmail.com>
2017-10-12 20:32:58 -0700

    Created action and reducer to fetch data with redux

---
+++ client/src/redux/modules/card_data.js
@@ -0,0 +1,52 @@
+//This file contains the actions and reducers for pulling the card data
+
+const FETCH_DATA = "FETCH_DATA";
+
+//Action
+
+export function fetchingData (itemsWithUsers) {
+    return {
+        type: FETCH_DATA,
+        payload: itemsWithUsers
+    };
+}
+
+    export function fetchCardData() {
+        return function (dispatch) {
+        Promise.all(['http://localhost:3001/items', 'http://localhost:3001/users'].map(url => (
+            fetch(url).then(response => response.json()))))
+            .then(json => {
+            const [item, users] = json;
+
+            const itemsWithUsers = item.map(item => {
+                return {
+                    id: item.id,
+                    title: item.title,
+                    description: item.description,
+                    imageUrl: item.imageUrl,
+                    tags: item.tags,
+                    createdOn: item.createdOn,
+                    available: item.available,
+                    borrower: item.borrower,
+                    user: users.find(user => user.id === item.itemOwner)
+                }
+            })             
+         })
+         dispatch(fetchingData(itemsWithUsers));
+        };
+    }
+
+//Reducer
+
+const initialState = {
+                 itemCardData: [],
+                 loading: true
+             };
+
+export default function (state = initialState, action) {
+    switch(action.type) {
+        case FETCH_DATA:
+            return action.payload
+    }
+    return state;
+}
---
+++ client/src/redux/root_reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+//import reducers here
+
+export default combineReducers({
+    // items,
+    // itemsHasErrored,
+    // itemsIsLoading,
+    // posts,
+    // postsHasErrored,
+    // postsIsLoading
+});
